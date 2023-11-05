a009640a54fab4003c3bffb4a907c4518e08e030
namangupta1010 <45966063+namangupta1010@users.noreply.github.com>
2023-11-05 18:27:17 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,15 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hey Naman");
+
+                System.out.println("Hey Naman");
+
+
+                System.out.println("Hey Naman");
+
+
+                System.out.println("Hey Naman");
+
     }
 }
 
